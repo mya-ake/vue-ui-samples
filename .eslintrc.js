3b7c45f20618b636529563f1c80b5c8978893796
mya-ake <make.app.ts@gmail.com>
2018-05-12 18:04:20 +0900

    Add base files

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential',
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    'arrow-parens': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'curly': 'error',
+    'default-case': 'error',
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
+    'no-console': 'warn',
+    'no-implicit-coercion': 'error',
+    'no-mixed-operators': 'error',
+    'no-var': 'error',
+    'prefer-arrow-callback': 'error',
+  },
+  globals: {},
+}
---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h1>Vue.js UI samples</h1>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
